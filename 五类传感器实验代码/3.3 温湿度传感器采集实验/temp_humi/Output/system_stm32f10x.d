dd3313e6ee5d57df9dd389eb6c59d5f46c019020
HuangChongqing <1756260160@qq.com>
2018-05-30 20:18:43 +0800

    compile

--- 五类传感器实验代码/3.3 温湿度传感器采集实验/temp_humi/Output/system_stm32f10x.d
+++ 五类传感器实验代码/3.3 温湿度传感器采集实验/temp_humi/Output/system_stm32f10x.d
@@ -1,7 +1,7 @@
 ..\output\system_stm32f10x.o: ..\CMSIS\system_stm32f10x.c
 ..\output\system_stm32f10x.o: ..\CMSIS\stm32f10x.h
 ..\output\system_stm32f10x.o: ..\CMSIS\core_cm3.h
-..\output\system_stm32f10x.o: D:\keil\ARM\ARMCC\bin\..\include\stdint.h
+..\output\system_stm32f10x.o: D:\Program Files\School\keil\ARM\ARMCC\bin\..\include\stdint.h
 ..\output\system_stm32f10x.o: ..\CMSIS\system_stm32f10x.h
 ..\output\system_stm32f10x.o: ..\USER\stm32f10x_conf.h
 ..\output\system_stm32f10x.o: ..\FWlib\inc\stm32f10x_gpio.h
